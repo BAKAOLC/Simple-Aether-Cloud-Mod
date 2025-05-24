700df6ed2d14e9768c93d203922ae36c9aa2648b
OLC <utsumabo@ritsukage.com>
2025-05-25 04:13:08 +0800

    移除红云类中的未使用导入，优化代码整洁性。

--- src/main/java/com/ritsukage/simple_aether_cloud/block/RedCloud.java
+++ src/main/java/com/ritsukage/simple_aether_cloud/block/RedCloud.java
@@ -4,8 +4,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
